1af4ef9880692323e8d073c4198c9fd2132678fc
dingmingxu <dingmingxu@bytedance.com>
2020-10-12 13:10:47 +0800

    nothing

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
-python demo/demo.py \
-      --config-file experiments/MS_R_50_1x_6.yaml \
+CUDA_VISIBLE_DEVICES=2 python demo/demo.py \
+      --config-file experiments/MS_R_50_1x_base_1.yaml \
       --input ../../data/COCO/coco/val2017/ \
-      --output /opt/tiger/toutiao/labcv/dmx_loop/data/visiual/condinst/coco_test/6
+      --output /opt/tiger/toutiao/labcv/dmx_loop/data/visiual/condinst/coco_test/base
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 OMP_NUM_THREADS=1 python tools/train_net.py \
-    --config-file experiments/MS_R_50_1x_7.yaml \
+    --config-file experiments/MS_R_50_1x_26.yaml \
     --eval-only \
     --num-gpus 4 \
-    OUTPUT_DIR /opt/tiger/toutiao/labcv/dmx_loop/data/visiual/condinst/7 \
-    MODEL.WEIGHTS /opt/tiger/toutiao/labcv/dmx_loop/checkpoints/condinst/MS_R_50_1x_7/model_0059999.pth
+    OUTPUT_DIR /opt/tiger/toutiao/labcv/dmx_loop/data/visiual/condinst/26 \
+    MODEL.WEIGHTS /opt/tiger/toutiao/labcv/dmx_loop/checkpoints/condinst/MS_R_50_1x_26/model_final.pth
