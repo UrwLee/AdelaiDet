973009cec37f1f5083697af20d184c2de5df62d3
dingmingxu <dingmingxu@bytedance.com>
2020-10-15 14:56:49 +0800

    fix gitignore

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 OMP_NUM_THREADS=1 python tools/train_net.py \
-    --config-file experiments/MS_R_50_1x_26.yaml \
+    --config-file experiments/MS_R_50_1x_28.yaml \
     --eval-only \
     --num-gpus 4 \
-    OUTPUT_DIR /opt/tiger/toutiao/labcv/dmx_loop/data/visiual/condinst/26 \
-    MODEL.WEIGHTS /opt/tiger/toutiao/labcv/dmx_loop/checkpoints/condinst/MS_R_50_1x_26/model_final.pth
+    OUTPUT_DIR /opt/tiger/toutiao/labcv/dmx_loop/data/visiual/condinst/28 \
+    MODEL.WEIGHTS /opt/tiger/toutiao/labcv/dmx_loop/checkpoints/condinst/MS_R_50_1x_28/model_final.pth
